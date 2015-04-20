12fbddb3746316657b012030efdee8d2b57a83cb
Matthew Barry <mbarry@tamu.edu>
2015-04-20 11:47:13 -0500

    @Provider annotation is not required for REST resource

--- bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/rest/HelloWorldResource.java
+++ bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/rest/HelloWorldResource.java
@@ -4,9 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.ext.Provider;
 
-@Provider
 @Path("/hello")
 public class HelloWorldResource
 {
