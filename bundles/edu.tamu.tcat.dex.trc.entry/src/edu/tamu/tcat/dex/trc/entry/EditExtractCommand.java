ccefd1025004a5caeb1ca4bec4e4670b7cc4687e
Matthew Barry <mbarry@tamu.edu>
2015-05-04 15:42:19 -0500

    Enrich EditExtractCommand API with methods to set properties on extract.

--- bundles/edu.tamu.tcat.dex.trc.entry/src/edu/tamu/tcat/dex/trc/entry/EditExtractCommand.java
+++ bundles/edu.tamu.tcat.dex.trc.entry/src/edu/tamu/tcat/dex/trc/entry/EditExtractCommand.java
@@ -1,9 +1,44 @@
 package edu.tamu.tcat.dex.trc.entry;
 
+import java.net.URI;
+import java.util.Set;
 import java.util.concurrent.Future;
+
+import org.w3c.dom.Document;
 
 public interface EditExtractCommand
 {
+   /**
+    * @param manuscript URI of the manuscript to which this extract belongs
+    */
+   void setManuscript(URI manuscript);
 
+   /**
+    * @param name of this extract's author (could differ from manuscript author)
+    */
+   void setAuthor(String author);
+
+   /**
+    * @param source URI of the bibliographic entry (e.g. play) from which this extract was taken
+    */
+   void setSource(URI source);
+
+   /**
+    * @param speakers A set of URIs corresponding to the characters who spoke the lines
+    *       recorded in this extract
+    */
+   void setSpeakers(Set<URI> speakers);
+
+   /**
+    * @param teiContent An XML Document representing the original TEI source of this extract
+    */
+   void setTEIContent(Document teiContent);
+
+   /**
+    * Persists the modifications made to the underlying DramaticExtract object.
+    *
+    * @return Future ID of the persisted object once persist actions have finished.
+    * @throws DramaticExtractException
+    */
    Future<String> execute() throws DramaticExtractException;
 }
