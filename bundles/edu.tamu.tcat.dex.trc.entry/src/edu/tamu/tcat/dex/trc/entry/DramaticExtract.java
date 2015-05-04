e0639e05de83fae471c587a533b795de336621b3
Matthew Barry <mbarry@tamu.edu>
2015-05-04 17:52:10 -0500

    Additional methods added to DEx TRC API.

--- bundles/edu.tamu.tcat.dex.trc.entry/src/edu/tamu/tcat/dex/trc/entry/DramaticExtract.java
+++ bundles/edu.tamu.tcat.dex.trc.entry/src/edu/tamu/tcat/dex/trc/entry/DramaticExtract.java
@@ -8,16 +8,25 @@ import org.w3c.dom.Document;
 /**
  * TODO add description of DEx from project startup documents
  */
-public interface DramaticExtract 
+public interface DramaticExtract
 {
+   /**
+    * @return A unique, persistent identifier for this extract
+    */
+   String getId();
 
 
    /**
-    * @return The URI of the bibliographic entry for the manuscript this extract was 
+    * @return The name of the extract's author. Usually the author of the manuscript.
+    */
+   String getAuthor();
+
+   /**
+    * @return The URI of the bibliographic entry for the manuscript this extract was
     * 		compiled in.
     */
    URI getManuscript();
-   
+
    /**
     * @return The URI of the bibliographic entry for the play or other source this
     * 		extract references.
@@ -25,20 +34,26 @@ public interface DramaticExtract
    URI getSource();
 
    /**
+    * @return A string representing the exact location within the source bibliographic entry where
+    *       this excerpt may be found.
+    */
+   String getSourceRef();
+
+   /**
     * @return A set of URIs for the biographical entries corresponding to the characters
-    * 		who spoke the lines recorded in this extract. 
+    * 		who spoke the lines recorded in this extract.
     */
    Set<URI> getSpeakers();
 
    /**
-    * @return The TEI encoded transcription of this extract as defined by the project 
-    *       guidelines. 
+    * @return The TEI encoded transcription of this extract as defined by the project
+    *       guidelines.
     */
    Document getTEIContent();    // TODO add link to description of guidelines
 
    /**
-    * @return A plain-text representation of the original content. This is extracted 
-    *       from the structured TEI content to remove spelling normalizations and 
+    * @return A plain-text representation of the original content. This is extracted
+    *       from the structured TEI content to remove spelling normalizations and
     *       expansion of abbreviations. The content will represent the extract as it
     *       originally appeared in the manuscript rather than in the play or other source
     *       material it was taken from.
@@ -47,9 +62,8 @@ public interface DramaticExtract
 
    /**
     * @return A normalized representation of the extract as plain-text. This representation
-    *       includes spelling normalizations and expansions of abbreviations to produce a 
+    *       includes spelling normalizations and expansions of abbreviations to produce a
     *       version of the extract that is easier for modern audiences to read and understand.
     */
    String getNormalizedContent();  // TODO this seems like it should be used internally, rather than part of the API.
-
 }
--- bundles/edu.tamu.tcat.dex.trc.entry/src/edu/tamu/tcat/dex/trc/entry/EditExtractCommand.java
+++ bundles/edu.tamu.tcat.dex.trc.entry/src/edu/tamu/tcat/dex/trc/entry/EditExtractCommand.java
@@ -9,19 +9,25 @@ import org.w3c.dom.Document;
 public interface EditExtractCommand
 {
    /**
+    * @param author Name of this extract's author (could differ from manuscript author)
+    */
+   void setAuthor(String author);
+
+   /**
     * @param manuscript URI of the manuscript to which this extract belongs
     */
    void setManuscript(URI manuscript);
 
    /**
-    * @param name of this extract's author (could differ from manuscript author)
-    */
-   void setAuthor(String author);
-
-   /**
     * @param source URI of the bibliographic entry (e.g. play) from which this extract was taken
     */
    void setSource(URI source);
+
+   /**
+    * @param sourceRef String representing the exact location within the bibliographic
+    *       source from which this extract was taken
+    */
+   void setSourceRef(String sourceRef);
 
    /**
     * @param speakers A set of URIs corresponding to the characters who spoke the lines
