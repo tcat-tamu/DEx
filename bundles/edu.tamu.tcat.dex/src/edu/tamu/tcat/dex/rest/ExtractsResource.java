1a82525b5dd27de8bb3e9e19d958f46ec0447f8d
Matthew Barry <mbarry@tamu.edu>
2015-06-01 12:04:44 -0500

    Implement fetch-by-ID methods in Extract and Manuscript REST resources.

--- bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/rest/ExtractsResource.java
+++ bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/rest/ExtractsResource.java
@@ -1,16 +1,38 @@
 package edu.tamu.tcat.dex.rest;
 
+import java.util.Objects;
+
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
+
+import edu.tamu.tcat.dex.trc.entry.DramaticExtract;
+import edu.tamu.tcat.dex.trc.entry.DramaticExtractException;
+import edu.tamu.tcat.dex.trc.entry.ExtractNotAvailableException;
+import edu.tamu.tcat.dex.trc.entry.ExtractRepository;
+import edu.tamu.tcat.trc.extract.dto.ExtractDTO;
 
 @Path("/extracts")
 public class ExtractsResource
 {
+   private ExtractRepository repo;
+
+   public void setRepo(ExtractRepository repo)
+   {
+      this.repo = repo;
+   }
+
+   public void activate()
+   {
+      Objects.requireNonNull(repo, "No repository specified");
+   }
 
    @GET
    @Path("/")
@@ -35,9 +57,21 @@ public class ExtractsResource
    @GET
    @Path("/{id}")
    @Produces(MediaType.APPLICATION_JSON)
-   public void get(@PathParam("id") String id)
+   public ExtractDTO get(@PathParam("id") String id)
    {
-      throw new UnsupportedOperationException("not yet implemented");
-   }
+      try
+      {
+         DramaticExtract extract = repo.get(id);
+         return ExtractDTO.create(extract);
+      }
+      catch (ExtractNotAvailableException e)
+      {
+         throw new NotFoundException("No such extract [" + id + "]", e);
+      }
+      catch (DramaticExtractException e)
+      {
+         throw new ServerErrorException("Unable to retrieve extract [" + id + "]", Status.INTERNAL_SERVER_ERROR, e);
+      }
 
+   }
 }
--- bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/rest/ManuscriptsResource.java
+++ bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/rest/ManuscriptsResource.java
@@ -2,15 +2,27 @@ package edu.tamu.tcat.dex.rest;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import edu.tamu.tcat.trc.entries.repo.NoSuchCatalogRecordException;
+import edu.tamu.tcat.trc.entries.types.bib.Work;
+import edu.tamu.tcat.trc.entries.types.bib.repo.WorkRepository;
+import edu.tamu.tcat.trc.extract.dto.ManuscriptDTO;
+
 @Path("/mss")
 public class ManuscriptsResource
 {
+   private WorkRepository repo;
+
+   public void setRepo(WorkRepository repo)
+   {
+      this.repo = repo;
+   }
 
    @GET
    @Path("/")
@@ -35,9 +47,17 @@ public class ManuscriptsResource
    @GET
    @Path("/{id}")
    @Produces(MediaType.APPLICATION_JSON)
-   public void get(@PathParam("id") String id)
+   public ManuscriptDTO get(@PathParam("id") String id)
    {
-      throw new UnsupportedOperationException("not yet implemented");
+         try
+         {
+            Work work = repo.getWork(id);
+            return ManuscriptDTO.create(work);
+         }
+         catch (NoSuchCatalogRecordException e)
+         {
+            throw new NotFoundException("Unable to find manuscript [" + id + "]");
+         }
    }
 
 }
