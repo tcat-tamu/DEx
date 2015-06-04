0d3279b5123283ddb1b39520183e9922904b593d
Matthew Barry <mbarry@tamu.edu>
2015-06-04 14:28:13 -0500

    Refactor service helper methods to a separate class.

---
+++ tests/edu.tamu.tcat.dex.psql.test/src/edu/tamu/tcat/dex/psql/test/ServiceHelper.java
@@ -0,0 +1,151 @@
+package edu.tamu.tcat.dex.psql.test;
+
+import java.net.URI;
+import java.util.Objects;
+import java.util.UUID;
+
+import edu.tamu.tcat.db.exec.sql.SqlExecutor;
+import edu.tamu.tcat.db.postgresql.exec.PostgreSqlExecutorService;
+import edu.tamu.tcat.db.provider.DataSourceProvider;
+import edu.tamu.tcat.dex.importer.DexImportService;
+import edu.tamu.tcat.dex.trc.entry.ExtractRepository;
+import edu.tamu.tcat.dex.trc.entry.tei.transform.ExtractManipulationUtil;
+import edu.tamu.tcat.osgi.config.ConfigurationProperties;
+import edu.tamu.tcat.trc.entries.core.IdFactory;
+import edu.tamu.tcat.trc.entries.types.bib.postgres.PsqlWorkRepo;
+import edu.tamu.tcat.trc.entries.types.bib.repo.WorkRepository;
+import edu.tamu.tcat.trc.entries.types.bio.postgres.PsqlPeopleRepo;
+import edu.tamu.tcat.trc.entries.types.bio.repo.PeopleRepository;
+import edu.tamu.tcat.trc.extract.postgres.PsqlDataSourceProvider;
+import edu.tamu.tcat.trc.extract.postgres.PsqlExtractRepo;
+import edu.tamu.tcat.trc.extract.search.ExtractSearchService;
+import edu.tamu.tcat.trc.extract.search.solr.DramaticExtractsSearchService;
+
+public class ServiceHelper
+{
+   // cached service instances
+   private static ConfigurationProperties config;
+   private static DataSourceProvider dataSourceProvider;
+   private static IdFactory idFactory;
+   private static SqlExecutor sqlExecutor;
+   private static PeopleRepository peopleRepository;
+   private static WorkRepository workRepository;
+   private static ExtractRepository extractRepository;
+   private static ExtractManipulationUtil extractManipulationUtil;
+   private static ExtractSearchService extractSearchService;
+   private static DexImportService importService;
+
+   public static ConfigurationProperties getConfig()
+   {
+      if (config == null)
+      {
+         BasicConfigurationProperties basicConfig = new BasicConfigurationProperties();
+         basicConfig.setProperty("db.postgres.url", "jdbc:postgresql://localhost:5432/dex");
+         basicConfig.setProperty("db.postgres.user", "postgres");
+         basicConfig.setProperty("db.postgres.pass", "1Password2");
+         basicConfig.setProperty("dex.xslt.tei.original", "/home/CITD/matt.barry/git/git.citd.tamu.edu/dex.deploy/xslt/tei-original.xsl");
+         basicConfig.setProperty("dex.xslt.tei.normalized", "/home/CITD/matt.barry/git/git.citd.tamu.edu/dex.deploy/xslt/tei-normalized.xsl");
+         basicConfig.setProperty("solr.api.endpoint", URI.create("http://mbarry.citd.tamu.edu:8983/solr/"));
+         basicConfig.setProperty("dex.solr.core", "extracts");
+
+         config = basicConfig;
+      }
+
+      Objects.requireNonNull(config, "config service not set");
+      return config;
+   }
+
+   public static DataSourceProvider getDataSourceProvider()
+   {
+      if (dataSourceProvider == null)
+      {
+         PsqlDataSourceProvider pdsp = new PsqlDataSourceProvider();
+
+         ConfigurationProperties config = getConfig();
+         pdsp.bind(config);
+
+         pdsp.activate();
+
+         dataSourceProvider = pdsp;
+      }
+
+      Objects.requireNonNull(dataSourceProvider, "data source provider service not set");
+      return dataSourceProvider;
+   }
+
+   public static SqlExecutor getSqlExecutor()
+   {
+      if (sqlExecutor == null)
+      {
+         PostgreSqlExecutorService psqlExecutor = new PostgreSqlExecutorService();
+
+         DataSourceProvider dsp = getDataSourceProvider();
+         psqlExecutor.bind(dsp);
+
+         psqlExecutor.activate();
+
+         sqlExecutor = psqlExecutor;
+      }
+
+      Objects.requireNonNull(sqlExecutor, "sql executor service not set");
+      return sqlExecutor;
+   }
+
+   public static ExtractRepository getExtractRepository()
+   {
+      if (extractRepository == null)
+      {
+         PsqlExtractRepo repo = new PsqlExtractRepo();
+
+         SqlExecutor executor = getSqlExecutor();
+         repo.setSqlExecutor(executor);
+
+         repo.activate();
+
+         extractRepository = repo;
+      }
+
+      Objects.requireNonNull(extractRepository, "extract repository service not set");
+      return extractRepository;
+   }
+
+   public static ExtractManipulationUtil getExtractManipulationUtil()
+   {
+      if (extractManipulationUtil == null)
+      {
+         extractManipulationUtil = new ExtractManipulationUtil();
+
+         ConfigurationProperties config = getConfig();
+         extractManipulationUtil.setConfiguration(config);
+
+         extractManipulationUtil.activate();
+      }
+
+      Objects.requireNonNull(extractManipulationUtil, "extract manipulation utility service not set");
+      return extractManipulationUtil;
+   }
+
+   public static ExtractSearchService getSearchService()
+   {
+      if (extractSearchService == null)
+      {
+         DramaticExtractsSearchService dess = new DramaticExtractsSearchService();
+
+         ConfigurationProperties config = getConfig();
+         dess.setConfig(config);
+
+         ExtractRepository repo = getExtractRepository();
+         dess.setRepo(repo);
+
+         ExtractManipulationUtil extractManipulationUtil = getExtractManipulationUtil();
+         dess.setExtractManipulationUtil(extractManipulationUtil);
+
+         dess.activate();
+
+         extractSearchService = dess;
+      }
+
+      Objects.requireNonNull(extractSearchService, "extract search service not set");
+      return extractSearchService;
+   }
+}
--- tests/edu.tamu.tcat.dex.psql.test/src/edu/tamu/tcat/dex/psql/test/TestExtractRepository.java
+++ tests/edu.tamu.tcat.dex.psql.test/src/edu/tamu/tcat/dex/psql/test/TestExtractRepository.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.net.URI;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -21,144 +20,19 @@ import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import edu.tamu.tcat.db.exec.sql.SqlExecutor;
-import edu.tamu.tcat.db.postgresql.exec.PostgreSqlExecutorService;
-import edu.tamu.tcat.db.provider.DataSourceProvider;
 import edu.tamu.tcat.dex.trc.entry.DramaticExtractException;
 import edu.tamu.tcat.dex.trc.entry.EditExtractCommand;
 import edu.tamu.tcat.dex.trc.entry.ExtractRepository;
 import edu.tamu.tcat.dex.trc.entry.Pair;
-import edu.tamu.tcat.dex.trc.entry.tei.transform.ExtractManipulationUtil;
-import edu.tamu.tcat.osgi.config.ConfigurationProperties;
-import edu.tamu.tcat.trc.extract.postgres.PsqlDataSourceProvider;
-import edu.tamu.tcat.trc.extract.postgres.PsqlExtractRepo;
-import edu.tamu.tcat.trc.extract.search.ExtractSearchService;
-import edu.tamu.tcat.trc.extract.search.solr.DramaticExtractsSearchService;
 
 public class TestExtractRepository
 {
-   // cached service instances
-   private static ConfigurationProperties config;
-   private static DataSourceProvider dataSourceProvider;
-   private static SqlExecutor sqlExecutor;
-   private static ExtractRepository extractRepository;
-   private static ExtractManipulationUtil extractManipulationUtil;
-   private static ExtractSearchService extractSearchService;
-
-   private static ConfigurationProperties getConfig()
-   {
-      if (config == null)
-      {
-         BasicConfigurationProperties basicConfig = new BasicConfigurationProperties();
-         basicConfig.setProperty("db.postgres.url", "jdbc:postgresql://localhost:5432/dex");
-         basicConfig.setProperty("db.postgres.user", "postgres");
-         basicConfig.setProperty("db.postgres.pass", "1Password2");
-         basicConfig.setProperty("dex.xslt.tei.original", "/home/CITD/matt.barry/git/git.citd.tamu.edu/dex.deploy/xslt/tei-original.xsl");
-         basicConfig.setProperty("dex.xslt.tei.normalized", "/home/CITD/matt.barry/git/git.citd.tamu.edu/dex.deploy/xslt/tei-normalized.xsl");
-         basicConfig.setProperty("solr.api.endpoint", URI.create("http://mbarry.citd.tamu.edu:8983/solr/"));
-         basicConfig.setProperty("dex.solr.core", "extracts");
-
-         config = basicConfig;
-      }
-
-      return config;
-   }
-
-   private static DataSourceProvider getDataSourceProvider()
-   {
-      if (dataSourceProvider == null)
-      {
-         PsqlDataSourceProvider pdsp = new PsqlDataSourceProvider();
-
-         ConfigurationProperties config = getConfig();
-         pdsp.bind(config);
-
-         pdsp.activate();
-
-         dataSourceProvider = pdsp;
-      }
-
-      return dataSourceProvider;
-   }
-
-   private static SqlExecutor getSqlExecutor()
-   {
-      if (sqlExecutor == null)
-      {
-         PostgreSqlExecutorService psqlExecutor = new PostgreSqlExecutorService();
-
-         DataSourceProvider dsp = getDataSourceProvider();
-         psqlExecutor.bind(dsp);
-
-         psqlExecutor.activate();
-
-         sqlExecutor = psqlExecutor;
-      }
-
-      return sqlExecutor;
-   }
-
-   private static ExtractRepository getExtractRepository()
-   {
-      if (extractRepository == null)
-      {
-         PsqlExtractRepo repo = new PsqlExtractRepo();
-
-         SqlExecutor executor = getSqlExecutor();
-         repo.setSqlExecutor(executor);
-
-         repo.activate();
-
-         extractRepository = repo;
-      }
-
-      return extractRepository;
-   }
-
-   private static ExtractManipulationUtil getExtractManipulationUtil()
-   {
-      if (extractManipulationUtil == null)
-      {
-         extractManipulationUtil = new ExtractManipulationUtil();
-
-         ConfigurationProperties config = getConfig();
-         extractManipulationUtil.setConfiguration(config);
-
-         extractManipulationUtil.activate();
-      }
-
-      return extractManipulationUtil;
-   }
-
-   private static ExtractSearchService getSearchService()
-   {
-      if (extractSearchService == null)
-      {
-         DramaticExtractsSearchService dess = new DramaticExtractsSearchService();
-
-         ConfigurationProperties config = getConfig();
-         dess.setConfig(config);
-
-         ExtractRepository repo = getExtractRepository();
-         dess.setRepo(repo);
-
-         ExtractManipulationUtil extractManipulationUtil = getExtractManipulationUtil();
-         dess.setExtractManipulationUtil(extractManipulationUtil);
-
-         dess.activate();
-
-         extractSearchService = dess;
-      }
-
-      return extractSearchService;
-   }
-
    @Test
    public void testSaveEntry() throws InterruptedException, ExecutionException, DramaticExtractException, SAXException, IOException, ParserConfigurationException
    {
-      ExtractRepository repo = getExtractRepository();
+      ExtractRepository repo = ServiceHelper.getExtractRepository();
       // ensure search service is started and listening to repository
-      getSearchService();
+      ServiceHelper.getSearchService();
 
       EditExtractCommand editCommand = repo.create(UUID.randomUUID().toString());
 
