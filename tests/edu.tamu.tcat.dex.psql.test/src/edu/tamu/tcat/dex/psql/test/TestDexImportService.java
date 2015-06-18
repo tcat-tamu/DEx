f9f9c3c1c2c7ed7a89e0972e8490baef13939eed
Matthew Barry <mbarry@tamu.edu>
2015-06-18 10:17:31 -0500

    Test importer on all manuscripts

--- tests/edu.tamu.tcat.dex.psql.test/src/edu/tamu/tcat/dex/psql/test/TestDexImportService.java
+++ tests/edu.tamu.tcat.dex.psql.test/src/edu/tamu/tcat/dex/psql/test/TestDexImportService.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.apache.commons.io.FilenameUtils;
 import org.junit.Test;
 
 import edu.tamu.tcat.dex.importer.DexImportException;
@@ -13,7 +14,16 @@ import edu.tamu.tcat.dex.importer.DexImportService;
 public class TestDexImportService
 {
    private static final String PEOPLE_PLAYS_TEI_PATH = "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/peopleandplays.xml";
-   private static final String MANUSCRIPT_TEI_PATH = "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/BodleianMSSancroft29.xml";
+   private static final String[] MANUSCRIPT_TEI_PATHS = {
+      "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/BLMSAdd10309.xml",
+      "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/DEx_Sample_BLAddMS22608.xml",
+      "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/BLMSAdd64078.xml",
+      "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/BLMSLansdowne1185.new.xml",
+      "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/BodleianMSSancroft29.xml",
+      "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/FolgerMSVa87_22Apr.xml",
+      "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/Harvard MS Fr. 487.xml",
+      "/home/CITD/matt.barry/Documents/Projects/dex/Sample Files/UChicago_MS824.xml"
+   };
 
    @Test
    public void testImport() throws DexImportException
@@ -31,38 +41,39 @@ public class TestDexImportService
 
    public boolean testImportPeopleAndPlays() throws DexImportException
    {
-      FileReader teiReader;
-      try
+      DexImportService importService = ServiceHelper.getImportService();
+
+      try (FileReader teiReader = new FileReader(PEOPLE_PLAYS_TEI_PATH))
       {
-         teiReader = new FileReader(PEOPLE_PLAYS_TEI_PATH);
+         importService.importPeopleAndPlaysTEI(teiReader);
       }
       catch (IOException e)
       {
-         fail("Unable to locate people and plays TEI XML file.");
+         fail("Unable to locate people and plays TEI XML file [" + PEOPLE_PLAYS_TEI_PATH + "].");
          return false;
       }
 
-      DexImportService importService = ServiceHelper.getImportService();
-      importService.importPeopleAndPlaysTEI(teiReader);
 
       return true;
    }
 
    public boolean testImportManuscript() throws DexImportException
    {
-      FileReader teiReader;
-      try
+      for (String filePath : MANUSCRIPT_TEI_PATHS)
       {
-         teiReader = new FileReader(MANUSCRIPT_TEI_PATH);
-      }
-      catch (IOException e)
-      {
-         fail("Unable to locate manuscript TEI XML file.");
-         return false;
-      }
+         String manuscriptId = FilenameUtils.getBaseName(filePath);
+         DexImportService importService = ServiceHelper.getImportService();
 
-      DexImportService importService = ServiceHelper.getImportService();
-      importService.importManuscriptTEI("BodleianMSSancroft29", teiReader);
+         try (FileReader teiReader = new FileReader(filePath))
+         {
+            importService.importManuscriptTEI(manuscriptId, teiReader);
+         }
+         catch (IOException e)
+         {
+            fail("Unable to locate manuscript TEI XML file [" + filePath + "].");
+            return false;
+         }
+      }
 
       return true;
    }
