04af6d87ce4274b988513bcb4e5df7a17bb2aef0
Matthew Barry <mbarry@tamu.edu>
2015-06-30 17:13:26 -0500

    try-edit/catch-create hack; refactor save actions into separate methods

--- bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/importer/DexImportService.java
+++ bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/importer/DexImportService.java
@@ -125,75 +125,7 @@ public class DexImportService
 
       manuscript.id = manuscriptId;
 
-      // save manuscript biblio entry
-      // TODO: use an "edit or create" method to edit an existing work by ID or create a work with
-      //       the given ID if it does not exist
-
-      EditWorkCommand editManuscriptCommand = worksRepo.create(manuscript.id);
-      Work manuscriptWork = ManuscriptImportDTO.instantiate(manuscript);
-      editManuscriptCommand.setAll(WorkDV.create(manuscriptWork));
-      editManuscriptCommand.execute();
-
-      // save extracts
-      for (ExtractImportDTO extract : manuscript.extracts)
-      {
-         extract.manuscript = ReferenceDTO.create(manuscript.id, manuscript.title);
-
-         extract.speakers = extract.speakerIds.parallelStream()
-               .map(id ->
-               {
-                  String name = null;
-                  try
-                  {
-                     name = peopleRepo.get(id).getCanonicalName().getDisplayName();
-                  }
-                  catch (Exception e)
-                  {
-                     // logger.log(Level.WARNING, "Unable to resolve name of referenced speaker [" + id + "] in [" + manuscriptId + "].", e);
-                     logger.log(Level.WARNING, "Unable to resolve name of referenced speaker [" + id + "] in [" + manuscriptId + "].");
-                  }
-
-                  return ReferenceDTO.create(id, name);
-               })
-               .filter(Objects::nonNull)
-               .collect(Collectors.toSet());
-
-         String sourceTitle = null;
-         try
-         {
-            // resolve extract source and set source display title on extract
-            Work source = worksRepo.getWork(extract.sourceId);
-            sourceTitle = source.getTitle().getCanonicalTitle().getFullTitle();
-
-            // set playwrights on extract
-            for (AuthorReference aRef : source.getAuthors()) {
-               String firstName = aRef.getFirstName();
-               String lastName = aRef.getLastName();
-               String name = (firstName == null ? "" : firstName) + " " + (lastName == null ? "" : lastName);
-               extract.playwrights.add(ReferenceDTO.create(aRef.getId(), name.trim()));
-            }
-         }
-         catch (NoSuchCatalogRecordException e)
-         {
-            // logger.log(Level.WARNING, "unable to resolve referenced play [" + extract.sourceId + "] in [" + manuscriptId + "].", e);
-            logger.log(Level.WARNING, "unable to resolve referenced play [" + extract.sourceId + "] in [" + manuscriptId + "].");
-         }
-         extract.source = ReferenceDTO.create(extract.sourceId, sourceTitle);
-
-         try
-         {
-            EditExtractCommand editExtractCommand = extractRepo.createOrEdit(extract.id);
-            editExtractCommand.setAll(ExtractDTO.instantiate(extract));
-
-            editExtractCommand.execute();
-         }
-         catch (DramaticExtractException e)
-         {
-            // TODO: accumulate and report at end.
-            // logger.log(Level.WARNING, "unable to import extract [" + extract.id + "] in [" + manuscriptId + "].", e);
-            logger.log(Level.WARNING, "unable to import extract [" + extract.id + "] in [" + manuscriptId + "].");
-         }
-      }
+      saveManuscript(manuscript);
    }
 
    public void importPeopleAndPlaysTEI(Reader tei) throws DexImportException
@@ -208,119 +140,254 @@ public class DexImportService
          throw new DexImportException("Unable to load TEI content", e);
       }
 
-      for (PlaywrightImportDTO playwright : importResult.playwrights.values())
+      importResult.playwrights.values().stream()
+         .filter(pw -> !pw.names.isEmpty())
+         .forEach(this::savePlaywright);
+
+      importResult.plays.values().stream()
+         .forEach(this::savePlay);
+
+      importResult.characters.values().stream()
+         .filter(c -> !c.names.isEmpty())
+         .forEach(this::saveCharacter);
+   }
+
+   /**
+    * Saves an imported manuscript
+    *
+    * @param manuscript
+    */
+   private void saveManuscript(ManuscriptImportDTO manuscript)
+   {
+      EditWorkCommand editManuscriptCommand = createOrEditWork(manuscript.id);
+
+      Work manuscriptWork = ManuscriptImportDTO.instantiate(manuscript);
+      editManuscriptCommand.setAll(WorkDV.create(manuscriptWork));
+      editManuscriptCommand.execute();
+
+      // TODO: delete extracts associated with manuscript prior to adding new ones or find a way to update existing extracts
+
+      for (ExtractImportDTO extract : manuscript.extracts)
       {
-         if (playwright.names.isEmpty()) {
-            logger.log(Level.WARNING, "skipping playwright with no names");
-            continue;
-         }
+         extract.manuscript = ReferenceDTO.create(manuscript.id, manuscript.title);
 
-         // save playwright
-         EditPersonCommand editCommand = peopleRepo.create(playwright.id);
+         extract.speakers = extract.speakerIds.parallelStream()
+               .map(id ->
+                     {
+                        String name = null;
+                        try
+                        {
+                           name = peopleRepo.get(id).getCanonicalName().getDisplayName();
+                        }
+                        catch (Exception e)
+                        {
+                           // logger.log(Level.WARNING, "Unable to resolve name of referenced speaker [" + id + "] in [" + manuscript.id + "].", e);
+                           logger.log(Level.WARNING, "Unable to resolve name of referenced speaker [" + id + "] in [" + manuscript.id + "].");
+                        }
 
-         List<PersonNameDTO> personNames = playwright.names.stream()
-            .map(n -> {
-               PersonNameDTO dto = new PersonNameDTO();
-               dto.displayName = n;
+                        return ReferenceDTO.create(id, name);
+                     })
+               .filter(Objects::nonNull)
+               .collect(Collectors.toSet());
 
-               return dto;
-            })
-            .collect(Collectors.toList());
-
-         // use first name listed as canonical name and all subsequent as other names
-         editCommand.setName(personNames.get(0));
-         editCommand.setNames(new HashSet<>(personNames.subList(1, personNames.size())));
-
-         editCommand.execute();
+         saveExtract(extract);
       }
+   }
 
-      for (PlayImportDTO play : importResult.plays.values())
+   /**
+    * Saves an imported extract
+    *
+    * @param extract
+    */
+   private void saveExtract(ExtractImportDTO extract)
+   {
+      String sourceTitle = null;
+      try
       {
-         // save plays
-         EditWorkCommand editCommand = worksRepo.create(play.id);
+         // resolve extract source and set source display title on extract
+         Work source = worksRepo.getWork(extract.sourceId);
+         sourceTitle = source.getTitle().getCanonicalTitle().getFullTitle();
 
-         editCommand.setType(TrcBiblioType.Play.toString());
-
-         editCommand.setTitles(play.titles.stream()
-               .map(t -> {
-                  TitleDV dto = new TitleDV();
-                  dto.title = t;
-                  dto.type = "Canonical";
-
-                  return dto;
-               })
-               .collect(Collectors.toList()));
-
-         editCommand.setAuthors(play.playwrightRefs.stream()
-               .map(ref -> {
-                  AuthorRefDV dto = new AuthorRefDV();
-                  dto.role = "Playwright";
-                  dto.authorId = ref.playwrightId;
-                  // HACK: storing author reference's full name in lastName field.
-                  dto.lastName = ref.displayName;
-
-                  return dto;
-               })
-               .collect(Collectors.toList()));
-
-         for (EditionDTO edition : play.editions)
+         // set playwrights on extract
+         for (AuthorReference aRef : source.getAuthors())
          {
-            EditionMutator editionMutator = editCommand.createEdition();
+            String firstName = aRef.getFirstName();
+            String lastName = aRef.getLastName();
+            String name = (firstName == null ? "" : firstName) + " " + (lastName == null ? "" : lastName);
+            extract.playwrights.add(ReferenceDTO.create(aRef.getId(), name.trim()));
+         }
+      }
+      catch (NoSuchCatalogRecordException e)
+      {
+         // logger.log(Level.WARNING, "unable to resolve referenced play [" + extract.sourceId + "] in [" + extract.manuscript.id + "].", e);
+         logger.log(Level.WARNING, "unable to resolve referenced play [" + extract.sourceId + "] in [" + extract.manuscript.id + "].");
+      }
+      extract.source = ReferenceDTO.create(extract.sourceId, sourceTitle);
 
-            TitleDV titleDTO = new TitleDV();
-            titleDTO.title = edition.title;
-            editionMutator.setTitles(Collections.singleton(titleDTO));
+      try
+      {
+         EditExtractCommand editExtractCommand = extractRepo.createOrEdit(extract.id);
+         editExtractCommand.setAll(ExtractDTO.instantiate(extract));
 
-            editionMutator.setAuthors(edition.editors.stream()
-                  .map(name -> {
-                     AuthorRefDV dto = new AuthorRefDV();
-                     dto.role = "Editor";
-                     dto.lastName = name;
+         editExtractCommand.execute();
+      }
+      catch (DramaticExtractException e)
+      {
+         // TODO: accumulate and report at end.
+         // logger.log(Level.WARNING, "unable to import extract [" + extract.id + "] in [" + extract.manuscript.id + "].", e);
+         logger.log(Level.WARNING, "unable to import extract [" + extract.id + "] in [" + extract.manuscript.id + "].");
+      }
+   }
+
+   /**
+    * Saves an imported play
+    *
+    * @param play
+    */
+   private void savePlay(PlayImportDTO play)
+   {
+      EditWorkCommand editCommand = createOrEditWork(play.id);
+
+      editCommand.setType(TrcBiblioType.Play.toString());
+
+      editCommand.setTitles(play.titles.stream()
+            .map(t ->
+                  {
+                     TitleDV dto = new TitleDV();
+                     dto.title = t;
+                     dto.type = "Canonical";
 
                      return dto;
                   })
-                  .collect(Collectors.toList()));
+            .collect(Collectors.toList()));
 
-            PublicationInfoDV pubInfoDTO = new PublicationInfoDV();
-            pubInfoDTO.publisher = edition.publisher;
-            pubInfoDTO.date = new DateDescriptionDTO();
-            pubInfoDTO.date.description = edition.date;
-            editionMutator.setPublicationInfo(pubInfoDTO);
+      editCommand.setAuthors(play.playwrightRefs.stream()
+            .map(ref ->
+                  {
+                     AuthorRefDV dto = new AuthorRefDV();
+                     dto.role = "Playwright";
+                     dto.authorId = ref.playwrightId;
+                     // HACK: storing author reference's full name in lastName field.
+                     dto.lastName = ref.displayName;
 
-            // HACK: link to online play edition is currently being stored in the summary field.
-            if (edition.link != null) {
-               editionMutator.setSummary(edition.link.toString());
-            }
+                     return dto;
+                  })
+            .collect(Collectors.toList()));
+
+      // TODO: remove all editions prior to adding new ones or find a way to update existing editions
+
+      for (EditionDTO edition : play.editions)
+      {
+         EditionMutator editionMutator = editCommand.createEdition();
+
+         TitleDV titleDTO = new TitleDV();
+         titleDTO.title = edition.title;
+         editionMutator.setTitles(Collections.singleton(titleDTO));
+
+         editionMutator.setAuthors(edition.editors.stream()
+               .map(name ->
+                     {
+                        AuthorRefDV dto = new AuthorRefDV();
+                        dto.role = "Editor";
+                        dto.lastName = name;
+
+                        return dto;
+                     })
+               .collect(Collectors.toList()));
+
+         PublicationInfoDV pubInfoDTO = new PublicationInfoDV();
+         pubInfoDTO.publisher = edition.publisher;
+         pubInfoDTO.date = new DateDescriptionDTO();
+         pubInfoDTO.date.description = edition.date;
+         editionMutator.setPublicationInfo(pubInfoDTO);
+
+         // HACK: link to online play edition is currently being stored in the summary field.
+         if (edition.link != null)
+         {
+            editionMutator.setSummary(edition.link.toString());
          }
-
-         editCommand.execute();
       }
 
-      for (CharacterImportDTO character : importResult.characters.values())
-      {
-         if (character.names.isEmpty()) {
-            logger.log(Level.WARNING, "skipping character with no names");
-            continue;
-         }
+      editCommand.execute();
+   }
 
-         // save characters
-         EditPersonCommand editCommand = peopleRepo.create(character.id);
+   /**
+    * Saves an imported playwright
+    *
+    * @param playwright
+    */
+   private void savePlaywright(PlaywrightImportDTO playwright)
+   {
+      EditPersonCommand editCommand = createOrEditPerson(playwright.id);
 
-         List<PersonNameDTO> personNameDTOs = character.names.stream()
-               .map(name -> {
+      List<PersonNameDTO> personNames = playwright.names.stream()
+         .map(n ->
+               {
                   PersonNameDTO dto = new PersonNameDTO();
-                  dto.displayName = name;
+                  dto.displayName = n;
 
                   return dto;
                })
-               .collect(Collectors.toList());
+         .collect(Collectors.toList());
+
+      // use first name listed as canonical name and all subsequent as other names
+      editCommand.setName(personNames.get(0));
+      editCommand.setNames(new HashSet<>(personNames.subList(1, personNames.size())));
+
+      editCommand.execute();
+   }
+
+   /**
+    * Saves an imported character
+    *
+    * @param character
+    */
+   private void saveCharacter(CharacterImportDTO character)
+   {
+      EditPersonCommand editCommand = createOrEditPerson(character.id);
+
+      List<PersonNameDTO> personNameDTOs = character.names.stream()
+            .map(name ->
+                  {
+                     PersonNameDTO dto = new PersonNameDTO();
+                     dto.displayName = name;
+
+                     return dto;
+                  })
+            .collect(Collectors.toList());
 
 
-         editCommand.setName(personNameDTOs.get(0));
-         editCommand.setNames(new HashSet<>(personNameDTOs.subList(1, personNameDTOs.size())));
+      editCommand.setName(personNameDTOs.get(0));
+      editCommand.setNames(new HashSet<>(personNameDTOs.subList(1, personNameDTOs.size())));
 
-         // TODO: save character-to-play relationships
-         editCommand.execute();
+      // TODO: save character-to-play relationships
+      editCommand.execute();
+   }
+
+
+   private EditPersonCommand createOrEditPerson(String id)
+   {
+      // HACK: I don't think this is what try/catch blocks are meant to do...
+      try
+      {
+         return peopleRepo.update(id);
+      }
+      catch (NoSuchCatalogRecordException e)
+      {
+         return peopleRepo.create(id);
+      }
+   }
+
+   private EditWorkCommand createOrEditWork(String id)
+   {
+      // HACK: I don't think this is what try/catch blocks are meant to do...
+      try
+      {
+         return worksRepo.edit(id);
+      }
+      catch (NoSuchCatalogRecordException e)
+      {
+         return worksRepo.create(id);
       }
    }
 
