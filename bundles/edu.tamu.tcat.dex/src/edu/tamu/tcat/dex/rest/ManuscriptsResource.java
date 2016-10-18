4d1bb4a426a2e7d2e44c050694c6abd510e45934
Neal Audenaert <neala@tamu.edu>
2016-10-18 18:14:07 -0500

    Support deletion of manuscripts

--- bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/rest/ManuscriptsResource.java
+++ bundles/edu.tamu.tcat.dex/src/edu/tamu/tcat/dex/rest/ManuscriptsResource.java
@@ -4,18 +4,22 @@ import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 
 import edu.tamu.tcat.dex.TrcBiblioType;
 import edu.tamu.tcat.dex.importer.DexImportService;
 import edu.tamu.tcat.dex.rest.v1.RepoAdapter;
 import edu.tamu.tcat.dex.rest.v1.RestApiV1;
+import edu.tamu.tcat.dex.trc.extract.ExtractRepository;
+import edu.tamu.tcat.dex.trc.extract.search.ExtractSearchService;
 import edu.tamu.tcat.trc.entries.repo.NoSuchCatalogRecordException;
 import edu.tamu.tcat.trc.entries.types.biblio.Work;
 import edu.tamu.tcat.trc.entries.types.biblio.repo.WorkRepository;
@@ -27,10 +31,18 @@ public class ManuscriptsResource
 
    private WorkRepository repo;
    private DexImportService importService;
+   
+   // required for deletion
+   private ExtractRepository extracts;
 
    public void setRepo(WorkRepository repo)
    {
       this.repo = repo;
+   }
+   
+   public void setExtracts(ExtractRepository repo)
+   {
+      this.extracts = repo;
    }
 
    public void setImportService(DexImportService importService)
@@ -43,6 +55,7 @@ public class ManuscriptsResource
       try 
       {
          Objects.requireNonNull(repo, "No works repository provided.");
+         Objects.requireNonNull(extracts, "No extracts repository provided.");
          Objects.requireNonNull(importService, "No import service provided.");
       }
       catch (Exception ex)
@@ -52,6 +65,30 @@ public class ManuscriptsResource
       }
    }
 
+   @DELETE
+   @Path("/{id}")
+   @Produces(MediaType.APPLICATION_JSON)
+   public Response remove(@PathParam("id") String id)
+   {
+      try 
+      {
+         // TODO should verify that this is in fact a MS
+         try {
+            repo.deleteWork(id);
+         } catch (IllegalArgumentException ex) {
+            // HACK: catch and suppress spurious exception. Thrown because no index service is configured for works.
+         }
+         extracts.removeByManuscriptId(id);
+         return Response.noContent().build();
+      }
+      catch (Exception ex)
+      {
+         logger.log(Level.SEVERE, "Failed to delete manuscript " + id, ex);
+         throw new IllegalStateException("Failed to delete manuscript " + id, ex);
+      }
+   }
+   
+   
    @GET
    @Path("/{id}")
    @Produces(MediaType.APPLICATION_JSON)
